c384d3581c61cd61aa5893ee8b4e8a4d9e35d61d
kharoub <mh.abukharoub@gmail.com>
2022-08-07 12:08:18 +0300

    Adding ngrx dev tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {StoreModule} from "@ngrx/store";
+import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -9,6 +10,7 @@ import {HeaderComponent} from './header/header.component';
 import {AuthInterceptor} from "./auth/auth.interceptor";
 import {SharedModule} from "./shared/shared.module";
 import * as fromApp from "./store/app.reducer";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -20,6 +22,7 @@ import * as fromApp from "./store/app.reducer";
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot(fromApp.appReducer),
+    StoreDevtoolsModule.instrument({logOnly: environment.production}),
     SharedModule
   ],
   providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
