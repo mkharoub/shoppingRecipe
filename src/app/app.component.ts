d72247a54ae0e1d8420ed299e1090afc9deb3f72
kharoub <mh.abukharoub@gmail.com>
2022-08-11 13:00:50 +0300

    angular universal app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, InjectionToken, OnInit, PLATFORM_ID} from '@angular/core';
+import {isPlatformBrowser} from "@angular/common";
 
 import {AuthService} from "./auth/auth.service";
 
@@ -8,10 +9,14 @@ import {AuthService} from "./auth/auth.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private authService: AuthService) {
+  constructor(
+    private authService: AuthService,
+    @Inject(PLATFORM_ID) private platformId: InjectionToken<any>) {
   }
 
   ngOnInit() {
-    this.authService.autoSignIn();
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.autoSignIn();
+    }
   }
 }
